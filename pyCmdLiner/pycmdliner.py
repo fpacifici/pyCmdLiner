a2abe2b7438629b2ac361f6783c6041a21a72caa
Filippo Pacifici <filippo.pacifici@gmail.com>
2012-08-30 17:24:45 +0200

    Parameters management

--- pyCmdLiner/pycmdliner.py
+++ pyCmdLiner/pycmdliner.py
@@ -16,6 +16,7 @@
 
 
 import sys
+import inspect
 
 
 class Pycmdliner(object):
@@ -61,13 +62,49 @@ class Pycmdliner(object):
         3) gets the method name for the command to be executed
         4) runs the command.
         '''
-        methodToExecute = self.__extractMethod__(inputParams)
+        self.printCaption()
+
         if businessObject == None and self.businessObject == None:
             raise Exception('Business object not provided')
         bObject = self.businessObject
         if businessObject != None:
             bObject = businessObject
-        return self.__runMethod__(bObject, methodToExecute)
+
+        methodToExecute = None
+        try:
+            methodToExecute = self.__extractMethod__(inputParams, bObject)
+        except InvalidInputError:
+            self.printDefaultUsage()
+            return
+
+        '''applies an offset to parameters list only if the first parameter is the name
+        of the command, otherwise passes the full parameters list'''
+        offset = 0 if 'defaultCommand' in self.configuration else 1
+
+        return self.__runMethod__(bObject, methodToExecute, inputParams[offset:])
+
+
+    def printCaption(self):
+        '''
+        Prints the application header as provided in the configuration.
+        '''
+        if 'appHeader' in self.configuration:
+            print self.configuration['appHeader']
+
+
+    def printDefaultUsage(self):
+        '''
+        Prints the usage of the application, either taking it from the configuration
+        or taking it from a business method, or taking it from a file.
+        '''
+        if 'usageString' in self.configuration:
+            print self.configuration['usageString']
+        elif 'usageMethod' in self.configuration:
+            print self.__runMethod__(self.businessObject, self.configuration['usageMethod'])
+        elif 'usageFile' in self.configuration:
+            f = open(self.configuration['usageFile'], 'r')
+            for line in f:
+                print line
 
 
     def __basicInputValidation__(self, inputParameters):
@@ -78,20 +115,54 @@ class Pycmdliner(object):
             raise InvalidInputError('Not Enough input parameters')
 
 
-    def __extractMethod__(self, inputParameters):
-        self.__basicInputValidation__(inputParameters)
-        command = inputParameters[0]
-        if 'commandMapping' not in self.configuration:
+    def __extractMethod__(self, inputParameters, businessObject):
+        if 'commandMapping' not in self.configuration and 'defaultCommand' not in self.configuration:
             raise Exception('Invalid configuration: commandMapping section not present')
-        if command in self.configuration['commandMapping']:
-            return self.configuration['commandMapping'][command]
+
         else:
-            raise InvalidInputError('Command %s does not exist' % command)
+            mapping = None
+            offset = 0
+            if 'commandMapping' in self.configuration:
+                self.__basicInputValidation__(inputParameters)
+                command = inputParameters[0]
+                if command in self.configuration['commandMapping']:
+                    mapping = self.configuration['commandMapping'][command]
+                    offset = 1
+                else:
+                    raise InvalidInputError('Command %s does not exist' % command)
+            elif 'defaultCommand' in self.configuration:
+                mapping = self.configuration['defaultCommand']
+
+            self.__checkParameters__(inputParameters[offset:],
+                mapping, businessObject)
+
+            return mapping
 
 
-    def __runMethod__(self, businessObject, methodName):
+    def __checkParameters__(self, inputParameters, objectMethodName, businessObject):
+        '''
+        Checks if the provided input parameters are valid in number for
+        the provided method.
+
+        First the minimum number of input parameter is checked. Then
+        the maximum number is checked as well.
+        '''
+        methodObject = getattr(businessObject, objectMethodName)
+        totalParamsNumber = len(inspect.getargspec(methodObject).args)
+        '''remove self'''
+        totalParamsNumber = totalParamsNumber - 1
+        nonMandatoryPs = inspect.getargspec(methodObject).defaults
+        nonMandatoryparamsNumber = 0 if nonMandatoryPs == None else len(nonMandatoryPs)
+
+        passedParamsNumber = len(inputParameters)
+
+        if passedParamsNumber < (totalParamsNumber - nonMandatoryparamsNumber) or passedParamsNumber > totalParamsNumber:
+            raise InvalidInputError('Invalid number of passed params: %d' % passedParamsNumber)
+
+
+    def __runMethod__(self, businessObject, methodName, params = []):
         method = getattr(businessObject, methodName)
-        return method()
+        return method(*params)
 
 
 class InvalidInputError(Exception):
